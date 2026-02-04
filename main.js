2d97b64076c0348f1a1cadae1a38b034bf172a28
Vanshi15605 <vanshikasharma15605@gmai.com>
2026-02-04 15:57:55 +0530

    First commit

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-console.log("LOrem")
-console.warn("k")
-let a=10
-console.log(a)
-a=20
-console.log(a)
-const g=1099
-console.log(g)
-g=12
-console.log(g)
+console.log("LOrem");
+console.warn("k");
+let a = 10;
+console.log(a);
+a = 20;
+console.log(a);
+const g = 1099;
+console.log(g);
+
+console.log(g);
+
+//math
+txt = "Hellooooo";
+console.log(txt.length);
+console.log(txt.toUpperCase);
+console.log(txt.toLowerCase);
+console.log(txt.include("html"));
